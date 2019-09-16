4445c252079a7ad65343b0451073f9522f95f7c8
Igor Pavloski <Igor.Pavloski@seavus.com>
2019-09-16 23:12:23 +0200

    Fonts, GlobalStyles, Images, GameScreens

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, StyleSheet, Button, Image } from "react-native";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+
+const GameOverScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <TitleText>The Game is Over!</TitleText>
+      <Image source={require("../assets/success.png")} />
+      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+      <BodyText>Number was: {props.userNumber}</BodyText>
+      <Button title="NEW GAME" onPress={props.onRestart} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default GameOverScreen;
